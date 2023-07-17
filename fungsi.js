05eb7d5e21ec15193dac99933bfe73f12ef72e14
Bleackh <faldhi.works@gmail.com>
2023-07-17 14:23:33 +0800

    merevisi commit perkalian

--- fungsi.js
+++ fungsi.js
@@ -6,8 +6,8 @@ console.log(add(1, 2));
 const subtract = (a, b) => a - b;
 console.log(subtract(1, 2));
 
-const multiplay = (a, b) => a * b;
-console.log(multiplay(2, 2));
+const multiplay_number = (a, b) => a * b;
+console.log(multiplay_number(2, 2));
 
 const divide = (a, b) => a / b;
 console.log(divide(4, 2));
